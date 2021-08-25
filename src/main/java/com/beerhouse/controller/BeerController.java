83e77d617d96f270261f5cab4e677296e69a6448
Augusto Neto <augusto.soliveiran@gmail.com>
2021-08-25 14:59:12 -0300

    Corrigindo identação

--- src/main/java/com/beerhouse/controller/BeerController.java
+++ src/main/java/com/beerhouse/controller/BeerController.java
@@ -29,65 +29,65 @@ import com.beerhouse.service.BeerService;
 
 @RestController
 public class BeerController {
-	
+
 	@Autowired
 	private BeerService beerService;
-	
+
 	@GetMapping(value = "/beers")
 	public List<Beer> findAll() {
 		return beerService.findAll();
 	}
-	
+
 	@PostMapping(value = "/beers")
 	public ResponseEntity<Void> save(@Valid @RequestBody Beer newBeer){
 		Beer beer = beerService.save(newBeer);
-		
+
 		//Location
 		//Get current resource url
 		URI uri = ServletUriComponentsBuilder.fromCurrentRequest()
 				.path("/{id}").buildAndExpand(beer.getId()).toUri();
-		
+
 		return ResponseEntity.created(uri).build();
 	}
-	
+
 	@GetMapping(value = "/beers/{id}")
 	public ResponseEntity<Beer> findById(@PathVariable Integer id) {
 		Beer beer = beerService.findById(id);
-		
+
 		return new ResponseEntity<Beer>(beer, HttpStatus.OK);
 	}
-	
+
 	@PutMapping(value = "/beers/{id}")
 	public ResponseEntity<Void> update(@PathVariable Integer id, @Valid @RequestBody Beer newBeer){		
 		beerService.update(id, newBeer);
-		
-        return new ResponseEntity<>(HttpStatus.OK);
+
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 	@PatchMapping(value = "/beers/{id}")
 	public ResponseEntity<Void> updatePartial(@PathVariable Integer id, @RequestBody Beer newBeer) {
 		beerService.updatePartial(id, newBeer);
-		
-        return new ResponseEntity<>(HttpStatus.OK);
+
+		return new ResponseEntity<>(HttpStatus.OK);
 	}
-	
+
 	@DeleteMapping(value = "/beers/{id}")
 	public ResponseEntity<Void> delete(@PathVariable Integer id) {
 		beerService.delete(id);
-		
-        return new ResponseEntity<>(HttpStatus.NO_CONTENT);
+
+		return new ResponseEntity<>(HttpStatus.NO_CONTENT);
 	}
-	
+
 	@ResponseStatus(HttpStatus.BAD_REQUEST)
-    @ExceptionHandler(MethodArgumentNotValidException.class)
-    public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
-        Map<String, String> errors = new HashMap<>();
-        ex.getBindingResult().getAllErrors().forEach((error) -> {
-            String fieldName = ((FieldError) error).getField();
-            String errorMessage = error.getDefaultMessage();
-            errors.put(fieldName, errorMessage);
-        });
-        return errors;
-    }
-	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public Map<String, String> handleValidationExceptions(MethodArgumentNotValidException ex) {
+		Map<String, String> errors = new HashMap<>();
+		ex.getBindingResult().getAllErrors().forEach((error) -> {
+			String fieldName = ((FieldError) error).getField();
+			String errorMessage = error.getDefaultMessage();
+			errors.put(fieldName, errorMessage);
+		});
+		return errors;
+	}
+
 }
